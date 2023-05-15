ed9ec7c15de52e68d4dc86ac6cb8d3ac38bbece9
sirmangler <thecatsmangler@gmail.com>
2023-05-15 15:53:17 +0100

    Properly refresh games on boot

--- pcsx2-winrt/App.cpp
+++ pcsx2-winrt/App.cpp
@@ -543,7 +543,7 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
 		WinRTHost::ProcessEventQueue();
 		if (VMManager::GetState() != VMState::Running)
 		{
-			GameList::Refresh(false, true);
+			GameList::Refresh(false);
 			ImGuiManager::InitializeFullscreenUI();
 
 			GetMTGS().WaitForOpen();
